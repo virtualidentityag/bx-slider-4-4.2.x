7f3077f8a108f32e48cda61e3bb35ab0d9c40e6f
Christoph Hummel <christoph.hummel@virtual-identity.com>
2019-04-01 10:19:25 +0200

    bugfix release chrome

--- src/js/jquery.bxslider.js
+++ src/js/jquery.bxslider.js
@@ -1116,10 +1116,6 @@
         slider.touch.start.x = touchPoints[0].pageX;
         slider.touch.start.y = touchPoints[0].pageY;
 
-        if (slider.viewport.get(0).setPointerCapture) {
-          slider.pointerId = orig.pointerId;
-          slider.viewport.get(0).setPointerCapture(slider.pointerId);
-        }
         // bind a "touchmove" event to the viewport
         slider.viewport.bind('touchmove MSPointerMove pointermove', onTouchMove);
         // bind a "touchend" event to the viewport
